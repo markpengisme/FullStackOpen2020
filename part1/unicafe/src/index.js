dbd5ef41e7130402da874806bfdaa0b3b03040ea
Mark Peng <markpengisme@gmail.com>
2021-02-18 14:13:52 +0800

    ex 1.7 unicafe - shows more statistics

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const ave = good * 1 + neutral * 0 + bad * -1
+  const postive = good / all 
 
   const handleGoodClick = () => {
     setGood(good + 1)
@@ -16,6 +19,8 @@ const App = () => {
   const handleBadClick = () => {
     setBad(bad + 1)
   }
+
+
   return (
     <div>
       <h1>Give FeedBack</h1>
@@ -26,6 +31,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {ave} </p>
+      <p>Postive: {postive}%</p>
     </div>
   )
 }
