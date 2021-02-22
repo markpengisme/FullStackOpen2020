a938e87ea27b95fa7c2c797dfb946a748489cc91
Mark Peng <markpengisme@gmail.com>
2021-02-22 21:41:38 +0800

    ex 2.1 courseinfo(init) - course component

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({header}) => {
+  return (
+    <>
+      <h1>{header}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.name} {props.exercise}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercise={parts[0].exercises}/>
+      <Part name={parts[1].name} exercise={parts[1].exercises}/>
+      <Part name={parts[2].name} exercise={parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
