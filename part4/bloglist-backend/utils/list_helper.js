ddae4ae7e868a0ce5293ca7a117aefcebd415e6e
Mark Peng <markpengisme@gmail.com>
2021-03-11 10:52:03 +0800

    ex 4.6 bloglist - most blogs unit tests with lodash

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const  _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -26,8 +28,33 @@ const favoriteBlog = (blogs) => {
   return reformatedBlogs.reduce(reducer, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) { return {} }
+  const mapBlogs = _.map(blogs, (blog) => {
+    return {
+      author: blog.author,
+      blogs: 1
+    }
+  })
+
+  const reducedBlogs = _.reduce(mapBlogs, (result, blog) => {
+    const index = _.findIndex(result, (b) => {
+      return b.author === blog.author
+    })
+
+    if (index === -1) {
+      return _.concat(result, blog)
+    } else {
+      result[index].blogs += blog.blogs
+      return result
+    }
+  }, {})
+  return _.maxBy(reducedBlogs, function(b) { return b.blogs })
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
