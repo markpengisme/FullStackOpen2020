5102790c135de00eae75fbef1ebe30ffefbaae4e
Mark Peng <markpengisme@gmail.com>
2021-05-11 14:30:57 +0800

    ex 4.18 bloglist - jwt token

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -3,6 +3,7 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
