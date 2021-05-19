afb57c7695ae4b7fe94e99ebcdaa20c8be9884f3
Mark Peng <markpengisme@gmail.com>
2021-05-19 22:58:27 +0800

    ex 5.14 bloglist - test('clicking the view button calls show details')

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './blog'
 
@@ -24,4 +24,26 @@ test('renders blog content', () => {
     expect(detail).toHaveStyle({ display: 'none' })
     expect(detail).toHaveTextContent(blog.title)
     expect(detail).toHaveTextContent(blog.url)
+})
+
+
+test('clicking the view button calls show details', () => {
+    const blog = {
+        title: 'Example.com',
+        author: 'Mark',
+        url: 'example.com',
+        likes: 87,
+    }
+
+    const component = render(<Blog blog={blog} />)
+    const detail = component.container.querySelector('.detail-blog')
+
+    expect(detail).toHaveStyle({ display: 'none' })
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(detail).not.toHaveStyle({ display: 'none' })
+    expect(detail).toHaveTextContent(blog.url)
+    expect(detail).toHaveTextContent(blog.likes)
 })
